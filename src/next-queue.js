13b6f46e5449d1fa829be00644e7614cfc014fb0
afeiship <1290657123@qq.com>
2017-10-11 11:02:52 +0800

    issue: callback for result

--- src/next-queue.js
+++ src/next-queue.js
@@ -3,31 +3,49 @@
   var global = global || this || self || window;
   var nx = global.nx || require('next-js-core2');
   var _ = nx.remove || require('next-remove');
+  var STATUS = {
+    LOAD:'load',
+    DONE:'done'
+  };
 
   var NxQueue = nx.declare('nx.Queue', {
     methods: {
       init: function (inArray) {
-        this._result = [];
         this._callbacks = inArray || [];
-        this.make();
+        this._thenCallback = nx.noop;
+        return this.make();
       },
       queue: function(inCallback){
         this._callbacks.push( inCallback );
-        this.make();
+        return this.make();
       },
       dequeue: function(inCallback){
         nx.remove( this._callbacks, [ inCallback ]);
-        this.make();
+        return this.make();
       },
       make: function(){
-        var next, self  = this;
-        this._iterations = [];
+        var next, self = this;
+        var length = this._callbacks.length;
+        var iterations = this._iterations = Array( length );
+        var result = [];
+        var done = function(data){
+          result.push( data );
+          self._thenCallback({ status: STATUS.LOAD, data: result });
+        };
+
         nx.each( this._callbacks, function(i, callback){
-          self._iterations[ i ] = function(){
-            next = self._iterations[ i + 1 ] || nx.noop;
+          iterations[ i ] = function(data){
+            next = iterations[ i + 1 ] || done;
             callback.call(self, next);
+            data && result.push(data);
+            self._thenCallback({ status: STATUS.DONE, data: result });
           };
-        },this);
+        });
+        return this;
+      },
+      then: function(inCallback){
+        this._thenCallback = inCallback;
+        return this;
       },
       start: function () {
         this._iterations[0]();
