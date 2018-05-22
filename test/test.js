24d143459e26b65b4acdbc6f9cf10a45e4f4f395
afeiship <1290657123@qq.com>
2018-05-22 21:43:17 +0800

    issue: update

--- test/test.js
+++ test/test.js
@@ -4,29 +4,33 @@ require('../src/next-queue');
 
 describe('next/Queue', function () {
 
-  var fn1 = function(next){
-    setTimeout(function(){
-      console.log(123,'1s');
-      next();
-    },1000);
+  var fn1 = function (next) {
+    setTimeout(function () {
+      console.log(123, '1s');
+      next({ result: 1 });
+    }, 1000);
   };
 
-  var fn2 = function(next){
-    setTimeout(function(){
-      console.log(456,'2s');
-      next();
-    },2000);
+  var fn2 = function (next) {
+    setTimeout(function () {
+      console.log(456, '2s');
+      next({ result: 2 });
+    }, 1000);
   };
 
-  var fn3 = function(next){
-    setTimeout(function(){
-      console.log(789,'3s');
-      next();
-    },3000);
+  var fn3 = function (next) {
+    setTimeout(function () {
+      console.log(789, '3s');
+      next({ result: 3 });
+    }, 1000);
   };
-
 
   var nxQueue = new nx.Queue( [fn1, fn2, fn3] );
-  nxQueue.start();
+  nxQueue.start().then(({ status, data})=>{
+    //console.log('end!', status, data);
+    if( status === 'done'){
+      console.log(data);
+    }
+  });
 
 });
